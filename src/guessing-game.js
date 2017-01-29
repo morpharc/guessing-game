97a92151263fd1e41718fdbdb94d5b828dde429e
Stanislav <morpharc@mail.ru>
2017-01-29 19:44:51 +0300

    guessing-game: all test

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+    	this.offset = Math.ceil( (this.max - this.min) /2 );
+    	this.val = this.min + this.offset;
+    	return this.val;
     }
 
     lower() {
-
+    	this.max = this.val;
     }
 
     greater() {
-
+		this.min = this.val;
     }
 }
 
